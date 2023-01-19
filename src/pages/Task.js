a13fe7cedb739e51e40dc13febcc856a2ebd52f2
cjmmendinueta <97084808+cjmmendinueta@users.noreply.github.com>
2023-01-19 20:51:31 +0800

    Revert "Task Page"

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,13 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import { IconButton, Button, Tooltip, Whisper } from "rsuite";
-import { Detail, Calendar, Time, Rate, Tag } from '@rsuite/icons'; // npm install rsuite @rsuite/icons
-import DescriptionModal from '../modals/Description_Modal';
-import DifficultyModal from '../modals/Difficulty_Modal';
-import TaskTypeModal from '../modals/Task_Type_Modal';
-import "rsuite/dist/rsuite.min.css";
-=======
->>>>>>> fb78e788cd44fac436036874a2d7601feb370c13
 import "./pages.css";
 import {
     Accordion,
@@ -19,67 +10,21 @@ import {
 import 'react-accessible-accordion/dist/fancy-example.css';
 import { MdEdit } from 'react-icons/md';
 import { RiDeleteBinLine } from 'react-icons/ri';
-<<<<<<< HEAD
-
-export default function Task() {
-  const ButtonStyle = { margin: "0px 5px" };
-  const [showButtons, setShowButtons] = useState(false);
-  const [showDeM, setShowDeM] = useState(false);
-  const [showDM, setShowDM] = useState(false);
-  const [showTTM, setShowTTM] = useState(false);
-=======
 
 
 export default function Task() {
   const [showButtons, setShowButtons] = useState(false);
->>>>>>> fb78e788cd44fac436036874a2d7601feb370c13
   return (
     <div className="TaskPage">
       <div className="LeftColumn">
         <div className="AddTaskForm">
-            <ul>
-            <input type="text" className="AddTask" placeholder="Add Task" />
-              <div>
-                <Whisper followCursor speaker={<Tooltip>Description</Tooltip>}>
-                  <IconButton icon={<Detail />} color="cyan" appearance="primary" style={ButtonStyle} 
-                    onClick={()=> {setShowDeM(true); console.log("Add Icon Pressed")}} />
-                </Whisper>
-
-                <Whisper followCursor speaker={<Tooltip>Deadline</Tooltip>}>
-                  <IconButton icon={<Calendar />} color="green" appearance="primary" style={ButtonStyle} />
-                </Whisper>
-                
-                <Whisper followCursor speaker={<Tooltip> </Tooltip>}>
-                  <IconButton icon={<Time />} color="violet" 
-                    appearance="primary" style={ButtonStyle} />
-                </Whisper>
-
-                <Whisper followCursor speaker={<Tooltip>Difficulty</Tooltip>}>
-                  <IconButton icon={<Rate />} color="yellow" appearance="primary" style={ButtonStyle}
-                    onClick={()=> {setShowDM(true); console.log("Add Icon Pressed")}} />
-                </Whisper>
-                
-                <Whisper followCursor speaker={<Tooltip>Task Type</Tooltip>}>
-                  <IconButton icon={<Tag />} color="blue" appearance="primary" style={ButtonStyle}
-                    onClick={()=> {setShowTTM(true); console.log("Add Icon Pressed")}} />
-                </Whisper>
-
-                <Button appearance="primary" color="orange" size="lg"
-                  style={{float: 'right'}}>ADD</Button>
-
-                <DescriptionModal
-                  showDeM={showDeM}
-                  setShowDeM={setShowDeM}
-                />
-                <DifficultyModal
-                  showDM={showDM}
-                  setShowDM={setShowDM}
-                />
-                <TaskTypeModal
-                  showTTM={showTTM}
-                  setShowTTM={setShowTTM}
-                />
-              </div>
+          <ul>
+            <li>
+              <input type="text" className="AddTask" placeholder="Add Task" />
+            </li>
+            <li>
+              
+            </li>
           </ul>
         </div>
         <div className="TaskList">
